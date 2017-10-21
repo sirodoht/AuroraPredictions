5e4bfd8cac2e5e6f856d7cb4f7fe71025657a281
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-10-21 20:20:13 +0300

    add oncreate methods on locations fragment

--- app/src/main/java/layout/LocationsFragment.java
+++ app/src/main/java/layout/LocationsFragment.java
@@ -2,9 +2,12 @@ package layout;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.uom.gr.aurorapredictions.R;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -44,6 +47,17 @@ public class LocationsFragment extends Fragment {
 
     public LocationsFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.locationsfragment, menu);
     }
 
     @Override
